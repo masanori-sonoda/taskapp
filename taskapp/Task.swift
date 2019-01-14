c56363db1774263abfacdf986092d83ec1a1ebdc
masanori.sonoda <masanori_sonoda@magocolo-design.com>
2019-01-14 12:54:14 +0900

    課題の要求事項を達成した。

---
+++ taskapp/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by Masanori Sonoda on 2018/12/13.
+//  Copyright © 2018 masanori.sonoda. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // カテゴリー
+    @objc dynamic var category = ""
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
